3ca5426db841ac722fabcda0454e35b44e40a2fd
zerolight59 <manusankaru10@gmail.com>
2025-01-25 00:32:01 +0530

    first commit

---
+++ test/unit/app.spec.ts
@@ -0,0 +1,21 @@
+import {bootstrap} from 'aurelia-bootstrapper';
+import {StageComponent} from 'aurelia-testing';
+import {PLATFORM} from 'aurelia-pal';
+
+describe('Stage App Component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = StageComponent
+      .withResources(PLATFORM.moduleName('app'))
+      .inView('<app></app>');
+  });
+
+  afterEach(() => component.dispose());
+
+  it('should render message', async () => {
+    await component.create(bootstrap);
+    const view = component.element;
+    expect(view.textContent.trim()).toBe('Hello World!');
+  });
+});
